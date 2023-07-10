3dd9085804517e0bdc12ee859a379c91a7deaf50
Priyanka <priyanka@aaruraatechnologies.com>
2023-07-10 19:36:53 +0530

    Product Table Added

--- table/table_collection.js
+++ table/table_collection.js
@@ -2,15 +2,20 @@ const menulist = {
     name: 'menulist',
     query: `PRIMARY KEY (uuid)`,
     queryList: [
-        {field: 'userUuid', type: 'varchar(50)', null: 'DEFAULT NULL'},
-        {field: 'uuid', type: 'varchar(50)', null: 'NOT NULL'},
-        {field: 'menuId', type: 'int', null: 'DEFAULT NULL'},
-        {field: 'menuName', type: 'varchar(20)', null: 'DEFAULT NULL'},
+        {field: 'userUuid', type: 'BINARY(16)', null: 'NOT NULL'},
+        {field: 'uuid', type: 'BINARY(16)', null: 'NOT NULL'},
+        {field: 'companyUuid', type: 'BINARY(16)', null: 'NOT NULL'},
+        {field: 'menuId', type: 'int', null: 'NOT NULL'},
+        {field: 'menuName', type: 'varchar(50)', null: 'NOT NULL'},
         {field: 'orderNo', type: 'int', null: 'DEFAULT NULL'},
-        {field: 'displayName', type: 'varchar(20)', null: 'DEFAULT NULL'},
-        {field: 'pathName', type: 'varchar(45)', null: 'DEFAULT NULL'},
-        {field: 'pathTemplate', type: 'varchar(45)', null: 'DEFAULT NULL'},
-        {field: 'isActive', type: 'int', null: 'DEFAULT NULL'}
+        {field: 'displayName', type: 'varchar(50)', null: 'NOT NULL'},
+        {field: 'pathName', type: 'varchar(150)', null: 'NOT NULL'},
+        {field: 'pathTemplate', type: 'varchar(150)', null: 'NOT NULL'},
+        {field: 'isActive', type: 'boolean', null: 'NOT NULL'},
+        {field: 'createdOn', type: 'datetime', null: 'NOT NULL'},
+        {field: 'createdBy', type: 'varchar(50)', null: 'NOT NULL'},
+        {field: 'lastModifiedOn', type: 'datetime', null: 'DEFAULT NULL'},
+        {field: 'lastModifiedBy', type: 'varchar(50)', null: 'DEFAULT NULL'}
     ]
 }
 
@@ -18,13 +23,16 @@ const userlist = {
     name: 'userlist',
     query: `PRIMARY KEY (uuid)`,
     queryList: [
-        {field: 'uuid', type: 'varchar(50)', null: 'NOT NULL'},
-        {field: 'username', type: 'varchar(15)', null: 'NOT NULL'},
+        {field: 'uuid', type: 'BINARY(16)', null: 'NOT NULL'},
+        {field: 'companyUuid', type: 'BINARY(16)', null: 'NOT NULL'},
+        {field: 'username', type: 'varchar(50)', null: 'NOT NULL'},
         {field: 'password', type: 'longtext', null: 'NOT NULL'},
-        {field: 'phoneNumber', type: 'varchar(15)', null: 'DEFAULT NULL'},
-        {field: 'email', type: 'varchar(30)', null: 'DEFAULT NULL'},
-        {field: 'createdOn', type: 'datetime', null: 'DEFAULT NULL'},
-        {field: 'lastModifiedOn', type: 'datetime', null: 'DEFAULT NULL'}
+        {field: 'phoneNumber', type: 'varchar(20)', null: 'NOT NULL'},
+        {field: 'email', type: 'varchar(50)', null: 'NOT NULL'},
+        {field: 'createdOn', type: 'datetime', null: 'NOT NULL'},
+        {field: 'createdBy', type: 'varchar(50)', null: 'NOT NULL'},
+        {field: 'lastModifiedOn', type: 'datetime', null: 'DEFAULT NULL'},
+        {field: 'lastModifiedBy', type: 'varchar(50)', null: 'DEFAULT NULL'}
     ]
 }
 
@@ -33,14 +41,49 @@ const versionupdate = {
     query: `PRIMARY KEY (version)`,
     queryList: [
         {field: 'version', type: 'decimal(10,0)', null: 'NOT NULL'},
-        {field: 'lastModifiedOn', type: 'datetime', null: 'DEFAULT NULL'}
+        {field: 'lastModifiedOn', type: 'datetime', null: 'DEFAULT NULL'},
+        {field: 'lastModifiedBy', type: 'varchar(50)', null: 'DEFAULT NULL'}
+    ]   
+}
+
+const company = {
+    name: 'company',
+    query: `PRIMARY KEY (uuid)`,
+    queryList: [
+        {field: 'uuid', type: 'BINARY(16)', null: 'NOT NULL'},
+        {field: 'companyName', type: 'varchar(100)', null: 'NOT NULL'},
+        {field: 'orgName', type: 'varchar(100)', null: 'NOT NULL'},
+        {field: 'createdOn', type: 'datetime', null: 'NOT NULL'},
+        {field: 'createdBy', type: 'varchar(50)', null: 'NOT NULL'},
+        {field: 'lastModifiedOn', type: 'datetime', null: 'DEFAULT NULL'},
+        {field: 'lastModifiedBy', type: 'varchar(50)', null: 'DEFAULT NULL'}
+    ]   
+}
+
+const product = {
+    name: 'product',
+    query: `PRIMARY KEY (uuid)`,
+    queryList: [
+        {field: 'uuid', type: 'BINARY(16)', null: 'NOT NULL'},
+        {field: 'serialNo', type: 'bigint(20)', null: 'NOT NULL'},
+        {field: 'productName', type: 'varchar(100)', null: 'NOT NULL'},
+        {field: 'productDescription', type: 'varchar(150)', null: 'DEFAULT NULL'},
+        {field: 'partNumber', type: 'varchar(100)', null: 'NOT NULL'},
+        {field: 'gst', type: 'DECIMAL(16,2)', null: 'DEFAULT NULL'},
+        {field: 'price', type: 'DECIMAL(16,2)', null: 'NOT NULL'},
+        {field: 'createdOn', type: 'datetime', null: 'NOT NULL'},
+        {field: 'createdBy', type: 'varchar(50)', null: 'NOT NULL'},
+        {field: 'lastModifiedOn', type: 'datetime', null: 'DEFAULT NULL'},
+        {field: 'lastModifiedBy', type: 'varchar(50)', null: 'DEFAULT NULL'}
     ]   
 }
 
 const all_tables = [
     menulist,
     userlist,
-    versionupdate
+    versionupdate,
+    company,
+    product
 ]
 
 module.exports = all_tables
